da7b74cd546397a3200e116c6183b9c4b3698692
Ousmane Konate <ousmanekonate@Ousmanes-MacBook-Pro.local>
2023-08-29 11:51:28 +0200

    feat: adding docker compose and dockerfile
    In order to work properly with both backend and frontend features and be able to test better,
    I've set up a docker compose

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
