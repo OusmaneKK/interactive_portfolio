4731335262d8c468e3b53d541cde772619ca5ec9
Administrator <admin@example.com>
2023-12-07 02:37:23 +0100

    fix: sidebar hiding text content issue

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { ChakraProvider } from '@chakra-ui/react'
 import { AuthProvider } from './components/AuthContext';
+import Navbar from './components/Navbar';
 
 
 
